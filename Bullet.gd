1269f6c18d0f203da9428ad1e38139a08f223e8a
josea122 <josea122@users.noreply.github.com>
2021-04-28 02:32:24 -0400

    Added gun and bullet

---
+++ Bullet.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+export var speed = 600
+
+func _physics_process(delta):
+	position += transform.x * speed * delta
--- PlayerShip.gd
+++ PlayerShip.gd
@@ -1,7 +1,10 @@
 extends KinematicBody2D
 
 export var speed = 10
+export var max_speed = 150
 export var rotation_speed = 3
+
+export (PackedScene) var Bullet
 
 var velocity = Vector2()
 var rotation_dir = 0
@@ -10,6 +13,9 @@ var screen_size
 
 func _ready():
 	screen_size = get_viewport_rect().size
+	
+	position.x = screen_size.x/2
+	position.y = screen_size.y/2
 
 func get_input():
 	rotation_dir = 0
@@ -20,16 +26,9 @@ func get_input():
 		rotation_dir -= 1
 	if Input.is_action_pressed("accelerate"):
 		velocity += Vector2(speed, 0).rotated(rotation)
-	print(velocity)
-
-func _physics_process(delta):
-	get_input()
-	
-	rotation += rotation_dir * rotation_speed * delta
-	velocity = move_and_slide(velocity)
-	
-	warp_player()
-		
+		velocity = velocity.clamped(max_speed)
+	if Input.is_action_just_pressed("shoot"):
+		shoot()
 
 func warp_player():
 	if position.x >= (screen_size.x + 10):
@@ -41,3 +40,16 @@ func warp_player():
 		position.y = 0
 	if position.y <= -10:
 		position.y = screen_size.y
+
+func shoot():
+	var b =  Bullet.instance()
+	owner.add_child(b)
+	b.transform = $Gun.global_transform
+
+func _physics_process(delta):
+	get_input()
+	
+	rotation += rotation_dir * rotation_speed * delta
+	velocity = move_and_slide(velocity)
+	
+	warp_player()
